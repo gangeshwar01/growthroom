187f1fa3e48e9d18689da358d481fc9aea1e91a4
gangeshwar01 <gangeshwarkumar001@gmail.com>
2025-06-25 14:41:07 +0530

    admin 4

--- accounts/custom_admin.py
+++ accounts/custom_admin.py
@@ -2,14 +2,13 @@ from django.contrib.admin import AdminSite
 from accounts.forms import StaffAuthenticationForm
 from django.utils.translation import gettext_lazy as _
 
-# Import all models
+# Import all models that exist
 from accounts.models import ProfileIssue, Profile
-from attendance.models import Attendance
-from training.models import TrainingSession
-from tasks.models import Task, Project
+from attendance.models import AttendanceRecord
+from training.models import TrainingSession, TrainingAttendance
+from tasks.models import Task, Project, Comment
 from performance.models import PerformanceReview
-from certificates.models import Certificate
-from employee_portal.models import EmployeeTask
+from certificates.models import CertificateRequest
 
 class MyAdminSite(AdminSite):
     site_header = _("Django administration")
@@ -22,20 +21,19 @@ custom_admin_site.register(ProfileIssue)
 custom_admin_site.register(Profile)
 
 # Register models from attendance app
-custom_admin_site.register(Attendance)
+custom_admin_site.register(AttendanceRecord)
 
 # Register models from training app
 custom_admin_site.register(TrainingSession)
+custom_admin_site.register(TrainingAttendance)
 
 # Register models from tasks app
 custom_admin_site.register(Task)
 custom_admin_site.register(Project)
+custom_admin_site.register(Comment)
 
 # Register models from performance app
 custom_admin_site.register(PerformanceReview)
 
 # Register models from certificates app
-custom_admin_site.register(Certificate)
-
-# Register models from employee_portal app
-custom_admin_site.register(EmployeeTask) 
+custom_admin_site.register(CertificateRequest) 
