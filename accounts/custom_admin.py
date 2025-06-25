661361ad53e1f67fda4428ecf783e79b65636a50
gangeshwar01 <gangeshwarkumar001@gmail.com>
2025-06-25 14:38:17 +0530

    admin 3

--- accounts/custom_admin.py
+++ accounts/custom_admin.py
@@ -1,7 +1,15 @@
 from django.contrib.admin import AdminSite
 from accounts.forms import StaffAuthenticationForm
 from django.utils.translation import gettext_lazy as _
-from .models import ProfileIssue
+
+# Import all models
+from accounts.models import ProfileIssue, Profile
+from attendance.models import Attendance
+from training.models import TrainingSession
+from tasks.models import Task, Project
+from performance.models import PerformanceReview
+from certificates.models import Certificate
+from employee_portal.models import EmployeeTask
 
 class MyAdminSite(AdminSite):
     site_header = _("Django administration")
@@ -9,5 +17,25 @@ class MyAdminSite(AdminSite):
 
 custom_admin_site = MyAdminSite(name='custom_admin')
 
-# Register your models here
-custom_admin_site.register(ProfileIssue) 
+# Register models from accounts app
+custom_admin_site.register(ProfileIssue)
+custom_admin_site.register(Profile)
+
+# Register models from attendance app
+custom_admin_site.register(Attendance)
+
+# Register models from training app
+custom_admin_site.register(TrainingSession)
+
+# Register models from tasks app
+custom_admin_site.register(Task)
+custom_admin_site.register(Project)
+
+# Register models from performance app
+custom_admin_site.register(PerformanceReview)
+
+# Register models from certificates app
+custom_admin_site.register(Certificate)
+
+# Register models from employee_portal app
+custom_admin_site.register(EmployeeTask) 
